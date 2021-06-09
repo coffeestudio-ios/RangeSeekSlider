17ceb9cfa53524410beedb7aaecf1794b91149c9
Art <artshesha@protonmail.com>
2021-06-09 20:27:28 +0300

    Update SPM

--- Package.swift
+++ Package.swift
@@ -1,5 +1,29 @@
+// swift-tools-version:5.1
 import PackageDescription
 
+//let package = Package(
+//    name: "RangeSeekSlider"
+//)
+
 let package = Package(
-    name: "RangeSeekSlider"
+    name: "RangeSeekSlider",
+    platforms: [
+        .iOS(.v9),
+    ],
+    products: [
+        .library(
+            name: "RangeSeekSlider",
+            targets: ["RangeSeekSlider"]),
+    ],
+    dependencies: [
+        // no dependencies
+    ],
+    targets: [
+        .target(
+            name: "RangeSeekSlider",
+            dependencies: []),
+//        .testTarget(
+//            name: "",
+//            dependencies: ["RangeSeekSlider"]),
+    ]
 )
